6b15a2b8ec09606f6633cbeee74105eea804b92f
VigneshwaranSadasivam <71323713+VigneshwaranSadasivam@users.noreply.github.com>
2022-01-29 10:15:48 +0530

    Update AppConstants.java

--- Nutrition-Assistant/src/main/java/com/vigneshwaran/NutritionAssistant/AppConstants.java
+++ Nutrition-Assistant/src/main/java/com/vigneshwaran/NutritionAssistant/AppConstants.java
@@ -4,5 +4,5 @@ import org.springframework.beans.factory.annotation.Value;
 
 public class AppConstants {
 	
-	public static final String API_KEY="b83d346435msh8fe6686c34fa340p1091cajsn36d5e66c8425";
+	public static final String API_KEY="";
 }
