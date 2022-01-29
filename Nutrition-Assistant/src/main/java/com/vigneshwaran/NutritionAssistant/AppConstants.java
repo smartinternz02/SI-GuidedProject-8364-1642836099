5eedbb89875a3e28236f5f54ae1ae4c84275c53b
VigneshwaranSadasivam <71323713+VigneshwaranSadasivam@users.noreply.github.com>
2022-01-29 09:33:40 +0530

    Add files via upload
    spring boot application for nutrition assistant

---
+++ Nutrition-Assistant/src/main/java/com/vigneshwaran/NutritionAssistant/AppConstants.java
@@ -0,0 +1,8 @@
+package com.vigneshwaran.NutritionAssistant;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class AppConstants {
+	
+	public static final String API_KEY="b83d346435msh8fe6686c34fa340p1091cajsn36d5e66c8425";
+}
---
+++ Nutrition-Assistant/src/main/java/com/vigneshwaran/NutritionAssistant/NutritionAssistantApplication.java
@@ -0,0 +1,14 @@
+package com.vigneshwaran.NutritionAssistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class NutritionAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutritionAssistantApplication.class, args);
+	}
+
+}
